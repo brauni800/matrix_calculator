4c4213ab755bd8cc4cb26e9a5c20b054ed5556f1
Braulio Betancourt <braulio.1796@gmail.com>
2021-02-11 23:40:49 -0600

    add: javadoc

--- src/main/java/app/controller/MatrixController.java
+++ src/main/java/app/controller/MatrixController.java
@@ -3,6 +3,13 @@ package app.controller;
 import app.model.Matrix;
 
 public class MatrixController {
+  /**
+   * Función para sumar dos matrices con las mismas dimensiones
+   * @param matrix1
+   * @param matrix2
+   * @return Suma de 2 matrices
+   * @throws Error Las matrices no son del mismo tamaño
+   */
   public Matrix sum(Matrix matrix1, Matrix matrix2) throws Error {
     if (matrix1.getWidth() == matrix2.getWidth() && matrix1.getHeight() == matrix2.getHeight()) {
       float[][] result = new float[matrix1.getWidth()][matrix1.getHeight()];
@@ -15,6 +22,12 @@ public class MatrixController {
     } else throw new Error("The matrices are off different sizes");
   }
 
+  /**
+   * Multiplicacion de una matriz con un escalar
+   * @param matrix
+   * @param scalar
+   * @return Matriz resultado de la multiplicación de una matriz con un escalar
+   */
   public Matrix multiplicationByScalar(Matrix matrix, int scalar) {
     float[][] result = new float[matrix.getWidth()][matrix.getHeight()];
     for (int coordX = 0; coordX < matrix.getWidth(); coordX++) {
@@ -25,6 +38,13 @@ public class MatrixController {
     return new Matrix(result);
   }
 
+  /**
+   * Multiplicación de dos matrices con el numero de columnas de la matriz 1 igual al numero de filas de la matriz 2
+   * @param matrix1 El número de columnas debe ser igual al número de filas de la otra matriz
+   * @param matrix2 El número de filas debe ser igual al número de columnas de la otra matriz
+   * @return Matriz resultado de la multiplicación de dos matrices
+   * @throws Error El número de columnas de la matriz 1 no coincide con el número de filas de la matriz 2
+   */
   public Matrix multiplicationOfTwoMatrices(Matrix matrix1, Matrix matrix2) throws Error {
     if (matrix1.getHeight() == matrix2.getWidth()) {
       float[][] result = new float[matrix1.getWidth()][matrix2.getHeight()];
@@ -39,6 +59,11 @@ public class MatrixController {
     } else throw new Error("The number of columns in the first matrix is different from the number of rows in the second matrix");
   }
 
+  /**
+   * Función para obtener la matriz inversa con el método de Gauss-Jordan de la matriz que se proporcione como parámetro
+   * @param matrix Matriz la cual se obtendrá su matriz inversa
+   * @return Matriz inversa
+   */
   public Matrix getInverseByGaussJordan(Matrix matrix) {
     float[][] identity = new float[matrix.getWidth()][matrix.getHeight()];
     for (int i = 0; i < matrix.getWidth(); i++) {
@@ -67,6 +92,15 @@ public class MatrixController {
     return new Matrix(identity);
   }
 
+  /**
+   * Resolución de un sistema de ecuaciones por método de Gauss-Jordan
+   * @param matrix Matriz que representa los valores de un sistema de ecuaciones.
+   * Cada fila representa una ecuación, las columnas de cada fila representa el valor de la variable independiente
+   * @param independentValues Valores independientes de la matriz. Representan la igualdad de cada ecuación en la matriz.
+   * La posición de los valores independientes deberán ser equivalente a la posición de la ecuación correspondiente.
+   * Ejemplo: ecuacion1 = resultado1, ecuacion2 = resultado2 | matriz = { { ...ecuacion1 }, { ...ecuacion2 } }, valoresIndependientes = { resultado1, resultado2 }
+   * @return Arreglo de valores que corresponden con su posición a cada variable del sistema de ecuaciones
+   */
   public float[] getSystemOfEquations(Matrix matrix, float[] independentValues) {
     for (int y = 0; y < matrix.getHeight(); y++) {
       // matrix.getData()[y][y2] debe ser igual a 1
